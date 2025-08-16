2a4762f7027b2bc974c37d225ef86a6a3bf0451b
Justin Hendrick <justinjhendrick@gmail.com>
2025-08-16 12:48:30 -0700

    add battery in top left corner

--- src/c/dashboard.c
+++ src/c/dashboard.c
@@ -13,14 +13,21 @@ static char s_buffer[BUFFER_LEN];
 typedef struct ClaySettings {
   GColor color_background;
   GColor color_time_text;
+  GColor color_corner_title;
+  GColor color_corner_value;
+  GColor color_separator;
   bool include_seconds;
+  uint8_t reserved[40]; // for later growth
 } __attribute__((__packed__)) ClaySettings;
 
 ClaySettings settings;
 
 static void default_settings() {
   settings.color_background = GColorOxfordBlue;
-  settings.color_time_text = GColorOrange;
+  settings.color_time_text = GColorChromeYellow;
+  settings.color_corner_title = GColorVividCerulean;
+  settings.color_corner_value = GColorChromeYellow;
+  settings.color_separator = GColorChromeYellow;
   settings.include_seconds = true;
 }
 
@@ -40,18 +47,51 @@ static void format_time(struct tm* now, char* buf, int buf_len) {
   }
 }
 
-static void draw_time(GContext* ctx, struct tm* now, GRect bounds) {
-  GPoint center = grect_center_point(&bounds);
-  GFont font = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
-  GRect bbox = rect_from_midpoint(center, GSize(bounds.size.w, 49 + 14));
+static void debug_bbox(GContext* ctx, GRect bbox) {
   if (BBOX_DEBUG) {
     graphics_context_set_stroke_width(ctx, 1);
     graphics_context_set_stroke_color(ctx, GColorWhite);
     graphics_draw_rect(ctx, bbox);
   }
+}
+
+static void draw_time(GContext* ctx, struct tm* now, GFont font, GRect bbox) {
+  debug_bbox(ctx, bbox);
   graphics_context_set_text_color(ctx, settings.color_time_text);
   format_time(now, s_buffer, BUFFER_LEN);
   graphics_draw_text(ctx, s_buffer, font, bbox, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+}
+
+static void draw_batt(GContext* ctx, GRect bbox) {
+  GRect upper = GRect(
+    bbox.origin.x,
+    bbox.origin.y,
+    bbox.size.w,
+    bbox.size.h / 3
+  );
+  debug_bbox(ctx, upper);
+  graphics_context_set_text_color(ctx, settings.color_corner_title);
+  snprintf(s_buffer, BUFFER_LEN, "%s", "Battery");
+  draw_text_midalign(ctx, s_buffer, upper, GTextAlignmentCenter, false);
+
+  GRect lower = GRect(
+    bbox.origin.x,
+    bbox.origin.y + bbox.size.h / 3,
+    bbox.size.w,
+    bbox.size.h * 2 / 3
+  );
+  debug_bbox(ctx, lower);
+  graphics_context_set_text_color(ctx, settings.color_corner_value);
+  BatteryChargeState bcs = battery_state_service_peek();
+  snprintf(s_buffer, BUFFER_LEN, "%d%%", bcs.charge_percent);
+  draw_text_topalign(ctx, s_buffer, lower, GTextAlignmentCenter, true);
+
+  graphics_context_set_stroke_width(ctx, 1);
+  graphics_context_set_stroke_color(ctx, settings.color_separator);
+  graphics_draw_line(ctx,
+    GPoint(lower.origin.x                + 10, lower.origin.y + lower.size.h),
+    GPoint(lower.origin.x + lower.size.w - 10, lower.origin.y + lower.size.h)
+  );
 }
 
 static void update_layer(Layer* layer, GContext* ctx) {
@@ -61,7 +101,19 @@ static void update_layer(Layer* layer, GContext* ctx) {
   GRect bounds = layer_get_bounds(layer);
   graphics_context_set_fill_color(ctx, settings.color_background);
   graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-  draw_time(ctx, now, bounds);
+
+  GPoint center = grect_center_point(&bounds);
+  GFont main_font = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
+  GRect time_bbox = rect_from_midpoint(center, GSize(bounds.size.w, 49 + 14));
+  draw_time(ctx, now, main_font, time_bbox);
+
+  int complication_avail_height = (bounds.size.h - time_bbox.size.h) / 2;
+  GPoint tl_center = GPoint(
+    bounds.origin.x + bounds.size.w / 4,
+    bounds.origin.y + complication_avail_height / 2
+  );
+  GRect tl_corner = rect_from_midpoint(tl_center, GSize(bounds.size.w / 2, complication_avail_height * 3 / 4));
+  draw_batt(ctx, tl_corner);
 }
 
 static void window_load(Window* window) {
@@ -95,7 +147,11 @@ static void save_settings() {
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
   Tuple *t;
   if ((t = dict_find(iter, MESSAGE_KEY_color_background       ))) { settings.color_background         = GColorFromHEX(t->value->int32); }
-  if ((t = dict_find(iter, MESSAGE_KEY_color_time_text        ))) { settings.color_time_text         = GColorFromHEX(t->value->int32); }
+  if ((t = dict_find(iter, MESSAGE_KEY_color_time_text        ))) { settings.color_time_text          = GColorFromHEX(t->value->int32); }
+  if ((t = dict_find(iter, MESSAGE_KEY_color_corner_title     ))) { settings.color_corner_title       = GColorFromHEX(t->value->int32); }
+  if ((t = dict_find(iter, MESSAGE_KEY_color_corner_value     ))) { settings.color_corner_value       = GColorFromHEX(t->value->int32); }
+  if ((t = dict_find(iter, MESSAGE_KEY_color_separator        ))) { settings.color_separator          = GColorFromHEX(t->value->int32); }
+  if ((t = dict_find(iter, MESSAGE_KEY_include_seconds        ))) { settings.include_seconds          = t->value->int8; }
   save_settings();
   // Update the display based on new settings
   layer_mark_dirty(window_get_root_layer(s_window));
--- src/c/utils.h
+++ src/c/utils.h
@@ -22,3 +22,72 @@ static int max(int a, int b) {
   }
   return b;
 }
+
+static void draw_text_valign(GContext* ctx, const char* buffer, GRect bbox, GTextAlignment align, bool bold, int valign) {
+  int h = bbox.size.h;
+  int font_height = 0;
+  int top_pad = 0;
+  GFont font;
+  if (h < 14) {
+    return;
+  } else if (h < 18) {
+    font_height = 9;
+    top_pad = 4;
+    if (bold) {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD);
+    } else {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+    }
+  } else if (h < 24) {
+    font_height = 11;
+    top_pad = 6;
+    if (bold) {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+    } else {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_18);
+    }
+  } else if (h < 28) {
+    font_height = 14;
+    top_pad = 9;
+    if (bold) {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+    } else {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
+    }
+  } else {
+    font_height = 18;
+    top_pad = 9;
+    if (bold) {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+    } else {
+      font = fonts_get_system_font(FONT_KEY_GOTHIC_28);
+    }
+  }
+  int bot_pad = h - font_height - top_pad;
+  int shift_up = (top_pad - bot_pad) / 2 + 1;
+  if (valign == 1) {
+    shift_up = top_pad;
+  } else if (valign == 2) {
+    shift_up = -bot_pad;
+  } else if (valign == 3) {
+    shift_up = 0;
+  }
+  GRect fixed_bbox = GRect(bbox.origin.x, bbox.origin.y - shift_up, bbox.size.w, bbox.size.h);
+  graphics_draw_text(ctx, buffer, font, fixed_bbox, GTextOverflowModeWordWrap, align, NULL);
+}
+
+static void draw_text_midalign(GContext* ctx, const char* buffer, GRect bbox, GTextAlignment align, bool bold) {
+  draw_text_valign(ctx, buffer, bbox, align, bold, 0);
+}
+
+static void draw_text_topalign(GContext* ctx, const char* buffer, GRect bbox, GTextAlignment align, bool bold) {
+  draw_text_valign(ctx, buffer, bbox, align, bold, 1);
+}
+
+static void draw_text_botalign(GContext* ctx, const char* buffer, GRect bbox, GTextAlignment align, bool bold) {
+  draw_text_valign(ctx, buffer, bbox, align, bold, 2);
+}
+
+static void draw_text_noalign(GContext* ctx, const char* buffer, GRect bbox, GTextAlignment align, bool bold) {
+  draw_text_valign(ctx, buffer, bbox, align, bold, 3);
+}
